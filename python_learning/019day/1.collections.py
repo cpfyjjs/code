cc90a3e7e2dc405dc8e7fffdcf5ad40fa15b2ac1
cpfyjjs <cpfyjjs@foxmail.com>
2018-09-06 10:39:10 +0800

    0906

--- python_learning/019day/1.collections.py
+++ python_learning/019day/1.collections.py
@@ -7,15 +7,89 @@
 @desc:
 '''
 
-import re
+from collections import namedtuple
 
-cont = '(1)-2*((60-30+(-40/5)*(9-2*3+7/8*99/4*2998+10*568/14))-(-4*3)/(16-3*2))'
+Person = namedtuple('person',['name','age','sex'])
 
-re_parentheses = re.compile(r'\(([^\(]*?)\)')
-ret = re_parentheses.findall(cont)
-c =re_parentheses.split(cont)
-print(c)
-re_digit = re.compile('(\d+)')
-for txt in ret:
-    c = re_digit.split(txt)
-    print (c)
+bob = Person('bob',12,'man')
+print(bob.name)
+print(bob)
+print(type(bob))
+# bob
+# person(name='bob', age=12, sex='man')
+# <class '__main__.person'>
+
+Point = namedtuple('point', ('X', 'Y'))
+point = Point(12, 34)
+print(point.X)
+print(point.Y)
+print(point)
+# 12
+# 34
+# point(X=12, Y=34)
+
+
+# 牌
+# 花色和数字
+Card = namedtuple('card',['suits','number'])
+c1 = Card('红桃',12)
+print(c1)
+# card(suits='红桃', number=12)
+
+from collections import deque
+q = deque([1,2,3,4])
+q.pop()
+print(q)
+q.clear()
+print(q)
+
+import queue
+q = queue.Queue()
+q.put(1)
+q.put(3)
+q.put(8)
+print(q)            # <queue.Queue object at 0x7fc278682e48>
+print(q.get())      # 1
+print(q.get())      # 1
+print(q.get())      # 1
+#print(q.get())      阻塞
+
+
+from queue import PriorityQueue
+from collections import OrderedDict
+
+od = OrderedDict([('a',1), ('b',2), ('c',3)])
+print(od)
+
+for k in od:
+    print (k)
+# OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+# a
+# b
+# c
+
+dit = {'k1': None, 'k2': None}
+
+from collections import defaultdict
+
+values = [1,2,3,4,5,612,34,45,5634,234,234]
+
+dedict = defaultdict(list)
+for i in values:
+    if i < 60:
+        dedict['k1'].append(i)
+    else:
+        dedict['k2'].append(i)
+
+print(dedict['k1'])
+# [1, 2, 3, 4, 5, 34, 45]
+
+d =defaultdict(lambda :5)
+print(d['a'])
+# 5
+
+from collections import Counter
+c = ['a', 'a', 'a', 'b', 'b']
+con = Counter(c)
+print(con)
+# Counter({'a': 3, 'b': 2})
---
+++ python_learning/019day/2.time.py
@@ -0,0 +1,15 @@
+#!/home/cpf/anaconda3/bin
+# encoding: utf-8
+'''
+@software: pycharm
+@file: 2.time.py
+@time: 18-9-6 上午10:36
+@desc:
+'''
+
+import time
+
+time.sleep(0.001)
+now = time.time()
+print(now)
+# 1536201459.4352944
