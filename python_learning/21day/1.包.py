ec1b104468c1b7140cca4758a832f7776d844fbe
cpfyjjs <cpfyjjs@foxmail.com>
2018-09-07 11:46:53 +0800

    0907

---
+++ python_learning/21day/1.包.py
@@ -0,0 +1,25 @@
+#!/home/cpf/anaconda3/bin
+# encoding: utf-8
+'''
+@software: pycharm
+@file: 1.包.py
+@time: 18-9-7 上午10:47
+@desc:
+'''
+
+#import glance.api.policy
+
+#   cmd.manage.main()
+
+import glance
+
+
+glance.api.policy.get()
+
+glance.api.policy.get()
+
+
+
+#glance.api.policy.get()
+
+
---
+++ python_learning/21day/__init__.py
@@ -0,0 +1,8 @@
+#!/home/cpf/anaconda3/bin
+# encoding: utf-8
+'''
+@software: pycharm
+@file: __init__.py.py
+@time: 18-9-6 下午8:14
+@desc:
+'''
